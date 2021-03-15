fdb56ba6fbc1cec885ecb4d2951a76353147bd8c
Matteo Gaddoni <gaddoz@gmail.com>
2021-03-15 03:01:46 +0100

    feat: chuck-norris api service

--- src/app/core/service/chuck-norris.service.ts
+++ src/app/core/service/chuck-norris.service.ts
@@ -1,15 +1,36 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Joke } from '../model/api/chuck-norris/joke';
 
 @Injectable()
 export class ApiChuckNorrisService {
   constructor(protected http: HttpClient) {}
 
-    getRandomJoke(): Observable<any> {
-      const url = 'https://api.chucknorris.io/jokes/random';
-      return this.http.get(url);
-    }
+  getRandomJoke(): Observable<Joke> {
+    const url = 'https://api.chucknorris.io/jokes/random';
+    return this.http.get<Joke>(url);
+  }
+
+  getJokesCategories(): Observable<string[]> {
+    const url = 'https://api.chucknorris.io/jokes/categories';
+    return this.http.get<string[]>(url);
+  }
+
+  getRandomJokeByCategory(category: string): Observable<string[]> {
+    const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
+    return this.http.get<string[]>(url);
+  }
+
+  getJokesBySearch(category: string): Observable<Joke[]> {
+    const url = `https://api.chucknorris.io/jokes/search?query={query}`;
+    return this.http.get<Joke[]>(url);
+  }
+
+  getJokesByCategory(category: string): Observable<Joke[]> {
+    return this.getJokesBySearch(category);
+  }
+
 }
 
 
--- src/app/module/main/lazy/chuck-norris/chuck-norris.component.ts
+++ src/app/module/main/lazy/chuck-norris/chuck-norris.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { forkJoin } from 'rxjs';
+import { Joke } from 'src/app/core/model/api/chuck-norris/joke';
 import { ApiChuckNorrisService } from 'src/app/core/service/chuck-norris.service';
 
 @Component({
@@ -6,13 +8,18 @@ import { ApiChuckNorrisService } from 'src/app/core/service/chuck-norris.service
   templateUrl: './chuck-norris.component.html',
 })
 export class ChuckNorrisComponent implements OnInit {
-  randomJoke = '';
+  randomJoke: Joke | undefined = undefined;
+  jokeCategories: string[] = [];
 
   constructor(apiChuckService: ApiChuckNorrisService) {
-    apiChuckService.getRandomJoke().subscribe((res: any) => {
-      console.log('🚀 ~ file: chuck-norris.component.ts ~ line 11 ~ ChuckNorrisComponent ~ x ~ res', res);
-      this.randomJoke = res.value;
-    });
+    forkJoin({
+      randomJoke: apiChuckService.getRandomJoke(),
+      jokeCategories: apiChuckService.getJokesCategories(),
+    }).subscribe({next: (res) => {
+      console.log('🚀 ~ file: chuck-norris.component.ts ~ line 19 ~ ChuckNorrisComponent ~ }).subscribe ~ res', res);
+      this.jokeCategories = res.jokeCategories;
+      this.randomJoke = res.randomJoke;
+    }});
   }
 
   ngOnInit(): void {}
