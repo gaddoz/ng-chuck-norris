01eca0a5abe9c661246d3d98f2c24d17e1166ae3
Matteo Gaddoni <gaddoz@gmail.com>
2021-04-16 04:52:00 +0200

    feat(json-placeholder): full load posts + comments + user

--- src/app/module/main/lazy/json-placeholder/view/main/main.component.ts
+++ src/app/module/main/lazy/json-placeholder/view/main/main.component.ts
@@ -1,48 +1,61 @@
 import { Component, ViewChild } from '@angular/core';
 import { Post, PostsService, Comment, User, UsersService } from 'generated-sources/api/jsonpl';
 import { CommentsService } from 'generated-sources/api/jsonpl/api/comments.service';
-import { Observable, of, Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { concat, forkJoin, Observable, of } from 'rxjs';
+import { concatAll, map, mergeMap, switchMap } from 'rxjs/operators';
 import { ModalComponent } from 'src/app/shared/modal/modal.component';
-interface SelectedPost extends Post {
+
+interface PostInfo extends Post {
   comments?: Comment[];
+  user?: User;
 }
 @Component({
   selector: 'app-json-placeholder-main',
   templateUrl: './main.component.html',
 })
 export class MainComponent {
-   @ViewChild(ModalComponent) refComments: ModalComponent;
-  posts$: Observable<Post[]>;
-  selectedPostId$ = new Subject<number>();
-  selectedPost: SelectedPost;
+  @ViewChild(ModalComponent) refComments: ModalComponent;
+  posts$: Observable<PostInfo[]>;
+  selectedPost: PostInfo;
   constructor(
     private postsService: PostsService,
     private commentService: CommentsService,
     private usersService: UsersService) {
 
-    // comments pipe wip
-    this.posts$ = this.postsService.listPosts();
+    this.posts$ = this.postsService.listPosts().pipe(
+      mergeMap((posts: Post[]) => {
+        if (posts.length > 0) {
+          return forkJoin(
+            posts.map((post: PostInfo) => this.commentService.getCommentsByPostId(post?.id||0).pipe(
+                map((comments: Comment[]) => {
+                  post.comments = comments;
+                  return post;
+                })
+              ))
+          );
+        }
+        return of([]);
+      }),
+      mergeMap((posts: PostInfo[]) => {
+        if (posts.length > 0) {
+          return forkJoin(
+            posts.map((post: PostInfo) => this.usersService.getUser(post?.userId||0).pipe(
+                map((user: User) => {
+                  post.user = user;
+                  return post;
+                })
+              ))
+          );
+        }
+        return of([]);
+      }),
+    );
+
   }
 
   showCommentsPosts(post: any){
-    this.selectedPostId$.next(post);
-    console.log('showCommentsPosts', post);
+    this.selectedPost = post;
     this.refComments.openModal();
-  }
-
-  getPostsComments(posts: any[]){
-      return posts.map(p =>
-        this.commentService.getCommentsByPostId(p?.id||0)
-        .pipe(map(c => ({post: p, comments: c})))
-      );
-  }
-
-  getPostsUsers(posts: any[]){
-      return posts.map(p =>
-        this.usersService.getUser(p?.post.userId||0)
-        .pipe(map(u => ({post: p.post, comments: p.comments, user: u})))
-      );
   }
 
 }
