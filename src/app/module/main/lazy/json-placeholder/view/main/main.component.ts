a8171c1968eef1950a044e081b9dfa76085f3cad
Matteo Gaddoni <gaddoz@gmail.com>
2021-04-16 03:13:11 +0200

    fix: clean up test pipes

--- src/app/module/main/lazy/json-placeholder/view/main/main.component.ts
+++ src/app/module/main/lazy/json-placeholder/view/main/main.component.ts
@@ -1,93 +1,34 @@
-import { Component } from '@angular/core';
-import { Post, PostsService, User, UsersService } from 'generated-sources/api/jsonpl';
+import { Component, ViewChild } from '@angular/core';
+import { Post, PostsService, Comment, User, UsersService } from 'generated-sources/api/jsonpl';
 import { CommentsService } from 'generated-sources/api/jsonpl/api/comments.service';
-import { forkJoin, Observable } from 'rxjs';
-import { filter, map, mergeMap, switchMap } from 'rxjs/operators';
+import { Observable, of, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ModalComponent } from 'src/app/shared/modal/modal.component';
+interface SelectedPost extends Post {
+  comments?: Comment[];
+}
 @Component({
   selector: 'app-json-placeholder-main',
   templateUrl: './main.component.html',
 })
 export class MainComponent {
-  data: any[] = [];
-  selectedPost: any= undefined;
+   @ViewChild(ModalComponent) refComments: ModalComponent;
+  posts$: Observable<Post[]>;
+  selectedPostId$ = new Subject<number>();
+  selectedPost: SelectedPost;
   constructor(
     private postsService: PostsService,
     private commentService: CommentsService,
     private usersService: UsersService) {
 
-// this.postsService.newPost({
-    //   title:'new title',
-    //   body:'new body',
-    // }).subscribe(res => {
-    //   console.log(res);
-    // });
+    // comments pipe wip
+    this.posts$ = this.postsService.listPosts();
+  }
 
-    // this.usersService.newUser({
-    //   name:'new user',
-    //   email:'newuser@sample.com',
-    // }).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.commentService.newComment({
-    //   postId: 1,
-    //   name: 'new user',
-    //   email: 'newuser@sample.com',
-    //   body: 'new comment',
-    // }).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.postsService.putPost(1,{
-    //   title: 'new title',
-    // }).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.postsService.getPost(101).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.postsService.getPost(1).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.postsService.listPosts().subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.commentService.listComments().subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.commentService.getCommentsByPostId(1).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.usersService.listUsers(1,9).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.commentService.getCommentsByPostId(1,1,1,'response').subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // this.postsService.listPosts('MED',1,3).subscribe(res => {
-    //   console.log(res);
-    // });
-
-    // full load posts -> comments + post -> user
-    this.postsService.listPosts()
-    .pipe(
-      mergeMap(ps => forkJoin(this.getPostsComments(ps))),
-    )
-    .pipe(
-      mergeMap(pc => forkJoin(this.getPostsUsers(pc))),
-    )
-    .subscribe(res=> {
-      console.log('full load res:',res);
-      this.data = res;
-    });
+  showCommentsPosts(post: any){
+    this.selectedPostId$.next(post);
+    console.log('showCommentsPosts', post);
+    this.refComments.openModal();
   }
 
   getPostsComments(posts: any[]){
