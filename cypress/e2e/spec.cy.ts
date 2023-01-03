fab92a92d6b22213978870d73d885e995e75645d
gaddoz <gaddoz@mailbox.org>
2023-01-03 23:20:37 +0100

    test: restore homepage test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,6 +1,16 @@
-describe('My First Test', () => {
-  it('Visits the initial project page', () => {
-    cy.visit('/')
-    cy.contains('app is running!')
-  })
-})
+it('loads homepage, open and close modal one and two', () => {
+  cy.visit('/');
+  cy.contains('ng-chuck-norris').should('be.visible');
+  cy.contains('button', ' Sample Clarity Modal One ')
+    .should('be.visible')
+    .click();
+  cy.contains('h3', 'Modal One').should('be.visible');
+  cy.contains('button', ' Ok ').should('be.visible').click();
+  cy.contains('h3', 'Modal One').should('not.exist');
+  cy.contains('button', ' Sample Clarity Modal Two ')
+    .should('be.visible')
+    .click();
+  cy.contains('h3', 'Modal Two').should('be.visible');
+  cy.contains('button', ' Ok ').should('be.visible').click();
+  cy.contains('h3', 'Modal Two').should('not.exist');
+});
