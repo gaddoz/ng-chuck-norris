be795d0b6f4310ce3e0a98eb3342a648da6bfafa
gaddoz <gaddoz@mailbox.org>
2023-01-03 23:17:28 +0100

    feat: ng upgrade to 13

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  videosFolder: 'cypress/videos',
-  screenshotsFolder: 'cypress/screenshots',
-  fixturesFolder: 'cypress/fixtures',
   e2e: {
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-    },
     baseUrl: 'http://localhost:4200',
+    supportFile: false,
+  },
+
+  component: {
+    devServer: {
+      framework: 'angular',
+      bundler: 'webpack',
+    },
+    specPattern: '**/*.cy.ts',
   },
 });
